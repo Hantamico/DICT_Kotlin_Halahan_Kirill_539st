d8e1fdfd043eaf10dd35bcaa5cbef54dbba6fc20
Kirill_Halahan <Galagan.k.a.01@gmail.com>
2024-03-14 18:58:24 +0200

    lb1_Done

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,52 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+import kotlin.system.exitProcess
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+fun greet(): String {
+    println("Hello! My name is DICT_Bot.")
+    println("Please, remind me your name.")
+    val name = readLine()!!
+    println("What a great name you have, $name!")
+    return name
+}
+
+fun guessAge() {
+    println("Let me guess your age.")
+    println("Enter remainders of dividing your age by 3, 5 and 7.")
+    val remainder3 = readLine()!!.toInt()
+    val remainder5 = readLine()!!.toInt()
+    val remainder7 = readLine()!!.toInt()
+    val age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+    println("Your age is $age!")
+}
+
+fun count() {
+    println("Now I will prove to you that I can count to any number you want.")
+    val num = readLine()!!.toInt()
+    for (i in 0..num) {
+        println("$i!")
     }
+}
+
+fun test() {
+    println("Let's test your programming knowledge.")
+    println("Why do we use methods?")
+    println("1. To repeat a statement multiple times.")
+    println("2. To decompose a program into several small subroutines.")
+    println("3. To determine the execution time of a program.")
+    println("4. To interrupt the execution of a program.")
+    while (true) {
+        val answer = readLine()!!
+        if (answer == "2") {
+            break
+        } else {
+            println("Please, try again.")
+        }
+    }
+    println("Congratulations, have a nice day!")
+}
+fun main() {
+    val name = greet()
+    guessAge()
+    count()
+    test()
+    exitProcess(0)
 }
